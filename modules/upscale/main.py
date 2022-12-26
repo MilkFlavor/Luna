ca5df4ae888915a304cca00a342bd897b54e36c2
MilkFlavor <milkflavor.git@gmail.com>
2022-12-26 12:01:56 -0500

    remake hai

--- modules/upscale/main.py
+++ modules/upscale/main.py
@@ -8,10 +8,10 @@ def main() -> int:
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     model = RealESRGAN(device, scale=4)
     model.load_weights('weights/RealESRGAN_x4.pth', download=True)
-    for i, image in enumerate(os.listdir("input")):
-        image = Image.open(f"input/{image}").convert('RGB')
+    for i, image in enumerate(os.listdir("inputs")):
+        image = Image.open(f"inputs/{image}").convert('RGB')
         sr_image = model.predict(image)
-        sr_image.save(f'output/{i}.png')
+        sr_image.save(f'results/{i}.png')
 
 
 if __name__ == '__main__':
