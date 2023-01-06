eaad29793541e69a7b7c61c3db2a4d37bb8d1a19
MilkFlavor <milkflavor.git@gmail.com>
2023-01-06 07:23:04 -0500

    init + git ignore

--- modules/split/main.py
+++ modules/split/main.py
@@ -1,4 +1,8 @@
-# importing the modules
+"""
+It takes all the files in the input folder, copies them to the output folder,
+and then splits them
+into two parts
+"""
 from logging import INFO, basicConfig, info
 import os
 import shutil
@@ -6,33 +10,35 @@ import lib
 
 basicConfig(format="[%(asctime)s] %(message)s", level=INFO)
 
-info('----- Luna by Milkflavor -----')
+def main():
+    """
+    It takes all the files in the input folder, copies them to the output folder,
+    and then splits them into two parts
+    """
+    info('----- Luna by Milkflavor -----')
 
-# Providing the folder path
-origin = os.getcwd() + '/input/'
-info(origin)
-target = os.getcwd() + '/output/'
-info (target)
+    # Providing the folder path
+    origin = os.getcwd() + '/input/'
+    info(origin)
+    target = os.getcwd() + '/output/'
+    info(target)
 
-# Fetching the list of all the files
-files = os.listdir(origin)
+    # Fetching the list of all the files
+    files = os.listdir(origin)
 
-# Fetching all the files to directory
-for file_name in files:
-    file_folder = target+file_name
-    # make a folder for each file
-    os.mkdir(file_folder)
-    info(file_name)
-    shutil.copy(origin+file_name, file_folder)
-info("Files are copied successfully")
+    # Fetching all the files to directory
+    for file_name in files:
+        info("----- Copying... " + file_name + " -----")
+        shutil.copy(origin+file_name, target+file_name)
+    print("Files are copied successfully")
 
-os.chdir(target)
+    os.chdir(target)
 
-for x in files:
-    info(f'----- Starting {x} -----')
-    os.chdir(target+x)
-    lib.slice(x, 2)
-    os.remove(x)
-    info(f'----- Finished {x} -----')
+    for img in files:
+        info("----- Splitting... " + img + " -----")
+        lib.slice(img, 2)
+        os.remove(img)
+        info("----- " + img + " split successfully! -----")
 
-info('----- Done -----')
+if __name__ == '__main__':
+    main()
